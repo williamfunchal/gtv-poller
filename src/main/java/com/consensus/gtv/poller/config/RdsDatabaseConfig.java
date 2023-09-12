9570197131775240d214856675ff0094649c4445
William Funchal <williamfunchal@gmail.com>
2023-09-12 08:39:50 +0300

    RDS config enabled

--- src/main/java/com/consensus/gtv/poller/config/RdsDatabaseConfig.java
+++ src/main/java/com/consensus/gtv/poller/config/RdsDatabaseConfig.java
@@ -1,9 +1,9 @@
 package com.consensus.gtv.poller.config;
 
 import org.springframework.boot.actuate.jdbc.DataSourceHealthIndicator;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 import javax.sql.DataSource;
 
@@ -13,7 +13,7 @@ public class RdsDatabaseConfig {
     private static final String DB_QUERY = "SELECT CURRENT_DATE FROM dual";
 
     @Bean
-    @Profile("local")
+    @ConditionalOnProperty(name = "poller.config.usage.enabled", havingValue = "true")
     public DataSourceHealthIndicator coreDbHealthIndicator(DataSource dataSource) {
         return new DataSourceHealthIndicator(dataSource, DB_QUERY);
     }
